31a2be0ac75959b82e27845f4ea9da556afcdea3
CameronB <cboudreau6238@gmail.com>
2016-04-05 21:07:40 -0400

    Updates to class and main file

---
+++ pickup_ai.py
@@ -0,0 +1,116 @@
+import os
+from time import sleep
+import sys
+
+class AIGame():
+
+    def print_text(self, a_string, a_is_slow):
+        if a_is_slow:
+            for words in a_string + "\n":
+                sys.stdout.write(words)
+                sys.stdout.flush()
+                sleep(.05)
+        else:
+            print(a_string)
+
+
+    def clear(self):
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
+
+
+    def choose_stick_count(self):
+        self.stick_count = input("How many sticks are there on the table initially?\n>")
+
+        if not self.check_initial_stick_count(self.stick_count):
+            print("Unacceptable entry - please choose between 10-100 sticks.")
+            return self.choose_stick_count()
+
+        return int(self.stick_count)
+
+
+    def check_initial_stick_count(self, stick_count):
+        return self.stick_count.isnumeric() and int(self.stick_count) in range(10, 101)
+
+
+    def get_new_stick_count(self, stick_count):
+        self.pickup_amount = self.get_pickup_amount(self.stick_count)
+
+        if not self.acceptable_pickup_amount(self.pickup_amount):
+            print("Unacceptable entry - please choose between 1-3 sticks")
+            return self.get_pickup_amount(stick_count)
+
+        return self.stick_count - int(self.pickup_amount)
+
+
+    def get_pickup_amount(self, stick_count):
+        if self.stick_count == 1:
+            self.pickup_amount = input("There is 1 stick left. You're so fucked...Go ahead and pick it up, loser.")
+        else:
+            self.pickup_amount = input("There are {} sticks left. How many would you like to take (1-3)?\n>".format(self.stick_count))
+
+        return self.pickup_amount
+
+
+    def acceptable_pickup_amount(self, pickup_amount):
+        return self.pickup_amount.isnumeric() and int(self.pickup_amount) in range(1, 4)
+
+
+    def check_loss(self, stick_count):
+        if self.stick_count <= 0:
+            return True
+
+    def go_again(self):
+        self.again = input("\nWould you like to go again? [y/N] \n")
+        if self.again.lower() == 'y':
+            return True
+
+    def turn_is_odd(self, turn_counter):
+        return self.turn_counter % 2 == 1
+
+
+    def print_loss(self, turn_counter):
+        if if turn_is_odd:(turn_counter):
+            print("FAIL! Get your shit together. Do you even pick up sticks bro?")
+        else:
+            print("You did it! You're smarter than a machine. Can't let them get cocky, can we?")
+
+
+    def turn_is_odd(self, turn_counter):
+        return self.turn_counter % 2 == 1
+
+
+    def go_again(self):
+        self.again = input("\nWould you like to go again? [y/N] \n")
+        if self.again.lower() == 'y':
+            return True
+
+
+    def ai_turn(stick_count):
+
+
+    def run_game(self):
+        self.clear()
+        self.stick_count = self.choose_stick_count()
+        self.turn_counter = 1
+        self.ai_dict = self.ai_dict.update(dict.fromkeys(list(range(1,101)), [1,2,3]))
+        while True:
+            self.clear()
+            if turn_is_odd::
+                print("You're up player one!")
+                self.stick_count = self.get_pickup_amount(self.stick_count)
+            else:
+                print("Please wait while the computer figures out how best to destroy you.")
+                self.print_text()
+
+            if self.check_loss(self.stick_count):
+                self.print_loss(turn_counter)
+                break
+
+            self.turn_counter += 1
+
+        if self.go_again():
+            self.main()
+        self.clear()
